28c27a1ca408e61b056752ecc15c9fe0287a5997
Eric Passmore <eric.passmore@gmail.com>
2023-08-04 07:17:58 -0700

    updated with eslint and prettier changes

--- src/miner.ts
+++ src/miner.ts
@@ -1,10 +1,9 @@
-import { Api, JsonRpc, RpcError } from "eosjs";
-import { JsSignatureProvider } from "eosjs/dist/eosjs-jssig";
-import "isomorphic-fetch"
-import { TextEncoder, TextDecoder } from "util";
+import { Api, JsonRpc } from 'eosjs';
+import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
+import 'isomorphic-fetch';
+import { TextDecoder, TextEncoder } from 'util';
 import { keccak256 } from 'ethereumjs-util';
-import {logger} from "./logger";
-
+import { logger } from './logger';
 
 export interface MinerConfig {
     privateKey?: string;
@@ -20,7 +19,7 @@ export default class EosEvmMiner {
     rpc: JsonRpc;
     api: Api;
     lastGetTableCallTime: number = 0;
-    gasPrice: string = "0x1";
+    gasPrice: string = '0x1';
     pushCount: number = 0;
 
     constructor(public readonly config: MinerConfig) {
@@ -39,54 +38,66 @@ export default class EosEvmMiner {
         });
     }
 
-    async eth_sendRawTransaction(params:any[]) {
-        let timeStarted = Date.now();
+    async eth_sendRawTransaction(params: any[]) {
+        const timeStarted = Date.now();
         this.pushCount++;
-        logger.info(`Pushing tx #${this.pushCount} to ${this.config.rpcEndpoints[this.currentRpcEndpoint]}`);
+        logger.info(
+            `Pushing tx #${this.pushCount} to ${this.config.rpcEndpoints[this.currentRpcEndpoint]}`
+        );
 
-        const rlptx:string = params[0].substr(2);
-        const sentTransaction = await this.api.transact(
-            {
-                actions: [
-                    {
-                        account: `eosio.evm`,
-                        name: "pushtx",
-                        authorization: [{
-                            actor : this.config.minerAccount,
-                            permission : this.config.minerPermission,
-                        }],
-                        data: { miner : this.config.minerAccount, rlptx }
-                    }
-                ],
-            },
-            {
-                blocksBehind: 3,
-                expireSeconds: this.config.expireSec || 60,
-            }
-        ).then(x => {
-            logger.info(`Pushed tx to ${this.config.rpcEndpoints[this.currentRpcEndpoint]}`);
-            logger.info(x);
-            return true;
-        }).catch(e => {
-            logger.error(`Error pushing #${this.pushCount} to ${this.config.rpcEndpoints[this.currentRpcEndpoint]}`);
-            logger.error(e);
+        const rlptx: string = params[0].substr(2);
+        await this.api
+            .transact(
+                {
+                    actions: [
+                        {
+                            account: `eosio.evm`,
+                            name: 'pushtx',
+                            authorization: [
+                                {
+                                    actor: this.config.minerAccount,
+                                    permission: this.config.minerPermission,
+                                },
+                            ],
+                            data: { miner: this.config.minerAccount, rlptx },
+                        },
+                    ],
+                },
+                {
+                    blocksBehind: 3,
+                    expireSeconds: this.config.expireSec || 60,
+                }
+            )
+            .then((x) => {
+                logger.info(`Pushed tx to ${this.config.rpcEndpoints[this.currentRpcEndpoint]}`);
+                logger.info(x);
+                return true;
+            })
+            .catch((e) => {
+                logger.error(
+                    `Error pushing #${this.pushCount} to ${
+                        this.config.rpcEndpoints[this.currentRpcEndpoint]
+                    }`
+                );
+                logger.error(e);
 
-            // Too many possible reasons for this to fail,
-            // not necessarily an issue with the node.
-            // this.swapRpcEndpoint();
+                // Too many possible reasons for this to fail,
+                // not necessarily an issue with the node.
+                // this.swapRpcEndpoint();
 
-            throw new Error("There was an error pushing this transaction from this EOS EVM miner.");
-        });
+                throw new Error(
+                    'There was an error pushing this transaction from this EOS EVM miner.'
+                );
+            });
 
         logger.info(`Latency for #${this.pushCount}: ${Date.now() - timeStarted}ms`);
 
-        return '0x'+keccak256(Buffer.from(rlptx, "hex")).toString("hex");
+        return '0x' + keccak256(Buffer.from(rlptx, 'hex')).toString('hex');
     }
 
-    async eth_gasPrice(params:any[]){
+    async eth_gasPrice() {
         const timeSinceLastCall = Date.now() - this.lastGetTableCallTime;
-        if (!(this.config.lockGasPrice && this.gasPrice !== "0x1") && timeSinceLastCall >= 1000 ) {
-
+        if (!(this.config.lockGasPrice && this.gasPrice !== '0x1') && timeSinceLastCall >= 1000) {
             try {
                 const result = await this.rpc.get_table_rows({
                     json: true,
@@ -95,20 +106,21 @@ export default class EosEvmMiner {
                     table: 'config',
                     limit: 1,
                     reverse: false,
-                    show_payer: false
+                    show_payer: false,
                 });
-                this.gasPrice = "0x" + parseInt(result.rows[0].gas_price).toString(16);
+                this.gasPrice = '0x' + parseInt(result.rows[0].gas_price).toString(16);
                 this.lastGetTableCallTime = Date.now();
-            } catch(e) {
-                logger.error("Error getting gas price from nodeos: " + e);
+            } catch (e) {
+                logger.error('Error getting gas price from nodeos: ' + e);
                 this.swapRpcEndpoint();
 
-                throw new Error("There was an error getting the gas price from this EOS EVM miner.");
+                throw new Error(
+                    'There was an error getting the gas price from this EOS EVM miner.'
+                );
             }
         }
 
-        logger.info("Gas price: " + this.gasPrice);
+        logger.info('Gas price: ' + this.gasPrice);
         return this.gasPrice;
     }
 }
-
